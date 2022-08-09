3a46362abd2a13d8e522428e5d48d0f3edbf6367
Serhii Bura <serhiibura@gamil.com>
2022-08-09 14:57:29 +0300

    Add file reading loop
    Signed-off-by: Serhii Bura <serhiibura@gamil.com>

--- words_counter.cpp
+++ words_counter.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main (int argc, char* argv[]) {
     std::cout << "You are welcome in the words counter util!" << std::endl;
+    std::fstream s(DATA_FILE, s.in);
+    if (!s.is_open()) {
+        std::cout << "failed to open " << "text.txt" << '\n';
+    } else {
+        std::string line;
+        while (std::getline(s, line)) {
+            std::cout << line << std::endl;
+        }
+    }
     return 0;
 }
